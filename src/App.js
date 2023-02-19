bf0cdfbcae324021c17e4505fc2dbba48ebba002
unknown <taurastaurenas@gmail.com>
2023-02-19 02:22:02 +0200

    simple valorant player comparison

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [tag, setTag] = useState("");
+  const [data, setData] = useState(null);
+  const [submittedName, setSubmittedName] = useState("");
+  const [submittedTag, setSubmittedTag] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmittedName(name);
+    setSubmittedTag(tag);
+    fetch(`https://api.henrikdev.xyz/valorant/v1/mmr-history/eu/${name}/${tag}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("No data found");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error(error);
+        setData(null);
+        setError("No data found");
+      });
+  };
+
+  let result = "";
+
+  if (data && data.data) {
+    let AMCount = 0;
+    let PMCount = 0;
+
+    data.data.forEach((item) => {
+      const suffix = item.date.slice(-2);
+
+      if (suffix === "AM") {
+        AMCount++;
+      } else if (suffix === "PM") {
+        PMCount++;
+      }
+    });
+
+    result = AMCount > PMCount ? "a night owl" : "a day playa";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <label>
+          Tag:
+          <input
+            type="text"
+            value={tag}
+            onChange={(event) => setTag(event.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {error && <div>{error}</div>}
+      {data && (
+        <div>
+          <p>
+            {submittedName}#{submittedTag} is {result}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,140 @@
+export const data = {
+  status: 200,
+  name: "taurimoN",
+  tag: "0420",
+  data: [
+    {
+      currenttier: 24,
+      currenttierpatched: "Immortal 1",
+      images: {
+        small:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/smallicon.png",
+        large:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/largeicon.png",
+        triangle_down:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangledownicon.png",
+        triangle_up:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangleupicon.png",
+      },
+      ranking_in_tier: 0,
+      mmr_change_to_last_game: -17,
+      elo: 2100,
+      date: "Monday, January 30, 2023 12:55 AM",
+      date_raw: 1675036537,
+    },
+    {
+      currenttier: 24,
+      currenttierpatched: "Immortal 1",
+      images: {
+        small:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/smallicon.png",
+        large:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/largeicon.png",
+        triangle_down:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangledownicon.png",
+        triangle_up:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangleupicon.png",
+      },
+      ranking_in_tier: 6,
+      mmr_change_to_last_game: -15,
+      elo: 2106,
+      date: "Monday, January 30, 2023 12:12 AM",
+      date_raw: 1675033926,
+    },
+    {
+      currenttier: 24,
+      currenttierpatched: "Immortal 1",
+      images: {
+        small:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/smallicon.png",
+        large:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/largeicon.png",
+        triangle_down:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangledownicon.png",
+        triangle_up:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangleupicon.png",
+      },
+      ranking_in_tier: 21,
+      mmr_change_to_last_game: -16,
+      elo: 2121,
+      date: "Sunday, January 29, 2023 12:52 AM",
+      date_raw: 1674949969,
+    },
+    {
+      currenttier: 24,
+      currenttierpatched: "Immortal 1",
+      images: {
+        small:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/smallicon.png",
+        large:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/largeicon.png",
+        triangle_down:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangledownicon.png",
+        triangle_up:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangleupicon.png",
+      },
+      ranking_in_tier: 37,
+      mmr_change_to_last_game: 21,
+      elo: 2137,
+      date: "Sunday, January 29, 2023 12:16 AM",
+      date_raw: 1674947762,
+    },
+    {
+      currenttier: 24,
+      currenttierpatched: "Immortal 1",
+      images: {
+        small:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/smallicon.png",
+        large:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/largeicon.png",
+        triangle_down:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangledownicon.png",
+        triangle_up:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangleupicon.png",
+      },
+      ranking_in_tier: 16,
+      mmr_change_to_last_game: -16,
+      elo: 2116,
+      date: "Saturday, January 28, 2023 11:28 PM",
+      date_raw: 1674944893,
+    },
+    {
+      currenttier: 24,
+      currenttierpatched: "Immortal 1",
+      images: {
+        small:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/smallicon.png",
+        large:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/largeicon.png",
+        triangle_down:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangledownicon.png",
+        triangle_up:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangleupicon.png",
+      },
+      ranking_in_tier: 32,
+      mmr_change_to_last_game: 22,
+      elo: 2132,
+      date: "Saturday, January 28, 2023 2:02 AM",
+      date_raw: 1674867733,
+    },
+    {
+      currenttier: 24,
+      currenttierpatched: "Immortal 1",
+      images: {
+        small:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/smallicon.png",
+        large:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/largeicon.png",
+        triangle_down:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangledownicon.png",
+        triangle_up:
+          "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/24/ranktriangleupicon.png",
+      },
+      ranking_in_tier: 10,
+      mmr_change_to_last_game: 19,
+      elo: 2110,
+      date: "Saturday, January 28, 2023 1:10 AM",
+      date_raw: 1674864635,
+    },
+  ],
+};
